a7111334f8fbb3d0c5b3ba20aac064d27b1334d6
crypto-any <aleaugusto.js+github@gmail.com>
2023-03-31 15:30:32 -0300

    Adicionar recurso de exportação da lista de extensões com ícones, numeração e fonte Open Sans
    - Buscar e exibir ícones das extensões (com fallback para imagem de espaço reservado)
    - Adicionar numeração à lista de extensões
    - Usar a fonte Open Sans para a página HTML gerada
    - Abrir links da página inicial das extensões em uma nova guia

--- background.js
+++ background.js
@@ -1,20 +1,61 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-  chrome.management.getAll(function (extList) {
+  chrome.management.getAll(async function (extList) {
     extList.sort(function (a, b) {
       return a.name.localeCompare(b.name); // ordena a lista por nome
     });
+
     var html =
-      '<html><head><title>Lista de Extensões</title></head><body><ul style="list-style:auto;">';
-    for (var i = 0; i < extList.length; i++) {
-      var ext = extList[i];
-      if (ext.type === 'extension' && ext.enabled) {
-        html +=
-          '<li><a href="' + ext.homepageUrl + '">' + ext.name + '</a></li>';
+      '<html><head><title>Lista de Extensões</title><link href="https://fonts.googleapis.com/css2?family=Open+Sans&display=swap" rel="stylesheet"><style>body { font-family: "Open Sans", sans-serif; }</style></head><body><ol>';
+
+    const getImageDataUrl = async function (url) {
+      try {
+        const response = await fetch(url);
+        const blob = await response.blob();
+        const reader = new FileReader();
+        return new Promise((resolve, reject) => {
+          reader.onloadend = () => resolve(reader.result);
+          reader.onerror = reject;
+          reader.readAsDataURL(blob);
+        });
+      } catch {
+        return 'https://via.placeholder.com/16';
       }
-    }
-    html += '</ul></body></html>';
-    var blob = new Blob([html], { type: 'text/html' });
-    var url = URL.createObjectURL(blob);
+    };
+
+    const extPromises = extList.map(async (ext, index) => {
+      if (ext.type === 'extension') {
+        const iconUrl =
+          ext.icons && ext.icons.length > 0 ? ext.icons[0].url : '';
+        const imageDataUrl = iconUrl
+          ? await getImageDataUrl(iconUrl)
+          : 'https://via.placeholder.com/16';
+
+        return (
+          '<li>' +
+          (ext.enabled ? '' : '<span style="opacity: 0.5;">') +
+          '<img src="' +
+          imageDataUrl +
+          '" style="width: 16px; height: 16px; vertical-align: middle; margin-right: 5px;">' +
+          '<a href="' +
+          ext.homepageUrl +
+          '" target="_blank">' +
+          ext.name +
+          '</a>' +
+          (ext.enabled ? '' : '</span>') +
+          '</li>'
+        );
+      } else {
+        return '';
+      }
+    });
+
+    const extHtmlList = await Promise.all(extPromises);
+
+    html += extHtmlList.join('');
+    html += '</ol></body></html>';
+
+    const blob = new Blob([html], { type: 'text/html' });
+    const url = URL.createObjectURL(blob);
     chrome.downloads.download({ url: url, filename: 'my-extensions.html' });
   });
 });
